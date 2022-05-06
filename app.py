da55aebbc2e3e3722b1c3f855ea215361bd37fd5
AgamalARM <75130776+AgamalARM@users.noreply.github.com>
2022-05-06 21:30:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,40 @@ def get_result(reading, trend):
         st.write('### High')
         color = st.color_picker('Alert', '#FFFF00')
         st.write('### the system will send yellow alerts')
+        
+        
+########################################################################
+
+from flask import *
+import json
+import time
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home_page():
+    data_set = {'page': 'Home',
+                'message': 'Success loaded Home page', 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+
+
+@app.route('/user/', methods=['GET'])
+def request_page():
+    # 127.0.0.1:port/user/?user=USER_NAME
+    user_query = str(request.args.get('user'))
+    data_set = {'page': 'Request',
+                'message': f'Success got the request for{user_query}', 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+
+
+if __name__ == '__main__':
+    app.run(port=7777)
+
 
 
 get_result(reading_value, trend_name)
