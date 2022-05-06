4c4acb08595a4e4da3f6422346d5c06c22b46aa4
AgamalARM <75130776+AgamalARM@users.noreply.github.com>
2022-05-06 22:09:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,35 +36,13 @@ def get_result(reading, trend):
         
 ########################################################################
 
-from flask import *
+
+import requests
 import json
-import time
 
-app = Flask(__name__)
+res = requests.get("https://2c91-156-194-204-201.eu.ngrok.io/")
+st.write(res)
 
-
-@app.route('/', methods=['GET'])
-def home_page():
-    data_set = {'page': 'Home',
-                'message': 'Success loaded Home page', 'Timestamp': time.time()}
-    json_dump = json.dumps(data_set)
-
-    return json_dump
-
-
-@app.route('/user/', methods=['GET'])
-def request_page():
-    # 127.0.0.1:port/user/?user=USER_NAME
-    user_query = str(request.args.get('user'))
-    data_set = {'page': 'Request',
-                'message': f'Success got the request for{user_query}', 'Timestamp': time.time()}
-    json_dump = json.dumps(data_set)
-
-    return json_dump
-
-
-# if __name__ == '__main__':
-#     app.run()
 
 
 
